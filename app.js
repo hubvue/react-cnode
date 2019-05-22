6dc2db57139868248c9a4e17606722e6e2649f3f
= <1191340528@qq.com>
2019-05-22 10:14:21 +0800

    添加404容错

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const Koa = require("koa");
 const app = new Koa();
 
 const config = require("./config/index");
-
+const ErrorHandle = require("./middlewares/ErrorHandle");
 //路由
 const router = require("./routes/index");
 router(app);
+
+//容错
+ErrorHandle.error(app);
+
 
 
 app.listen(config.port,config.href,()=> {
---
+++ middlewares/ErrorHandle.js
@@ -0,0 +1,15 @@
+const errorHandle = {
+    error(app,logger) {
+
+        app.use(async (ctx,next) => {
+            await next();
+            if(404 !== ctx.status){
+                return ;
+            }
+            ctx.status = 200;
+            ctx.body = '<script type="text/javascript" src="//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js" charset="utf-8"></script>';
+        })
+    }
+};
+
+module.exports = errorHandle;
